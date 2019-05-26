d79fe22261077a056f45244a7e6b3394b741845a
DX2DigitalGroup <dbuihien@my.bcit.ca>
2019-05-25 21:21:25 -0700

    updates

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-if [ -n "$BOOEYSFILES" ]; then
-	if [ -d "$BOOEYSFILES" ]; then	
-		if [ ! -f "$BOOEYSFILES/cdandls.sh" ]; then
-			cp ./cdandls.sh $BOOEYSFILES/;
-		fi
-#	else
-#		BOOEYSFILES="$HOME/.booeysays/files";
-#		mkdir -p "$BOOEYSFILES";
-#		cd $BOOEYSFILES;
-#		git clone 
-	fi
-fi
-
-if [ -f ~/.66mhzrc ]; then
-	echo -e '''# CDANDLS #
-\t. "$BOOEYSFILES/cdandls.sh";
-# /CDANDLS #''' >> ~/.66mhzrc;
+if [ ! $BOOEYSAYS ] || [ ! $BOOEYSBIN ]; then
+	function presentoption(){
+		echo -e "This system is not been prep'd for any DX2DG's scripts.\n\nWould you like to:\n\n\t1)	Copy the script into the system's current bin directory and use it that way.\n\n\tor...\n\n2)	Prep the system and install the script the way it was meant to be installed.\n\n"
+		read -p '[1/2] :  ' -n 1 -r installchoice
+		case $installchoice in
+		1)
+			install_noprep
+			;;
+		2)
+			prep_then_install
+			;;
+		*)
+			echo -e "Error! Invalid option"
+			unset installchoice
+			presentoption
+			;;
+		esac
+		unset installchoice
+	}
+	presentoption;
 fi
---
+++ prep/prep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+############### prep.sh ################
+##
+## [DATE]	Wed Mar  6 19:35:34 PST 2019
+## [FILE]	prep.sh
+## [DESC]	
+## [FUNC]	NA
+## [LANG]	Bourne Again Shell - BASH Script
+## [INST]	NA
+## [EXEC]	Syntax:
+##
+##
+## [XTRA]	NA
+##
+## [PKGE]	NA
+##
+########### DX2 Digital Media Group ###########
+

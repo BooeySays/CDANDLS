38ccd27b2a8bedfb802f5a0e3ba618b370ca92fd
BooeySays <pi@kali>
2019-08-08 15:20:14 +1000

    final

--- cdandls.sh
+++ cdandls.sh
@@ -8,32 +8,9 @@
 #	into the dir.
 # Inst: Add cdandls.sh to autoload script to load cd function
 
-__version__=1.1
+__version__=2.0
 function cd(){
-if [ $# == '0' ]; then
-	echo -e "Function missing ARG\nRun with --help for more information";
-elif [ $1 == '--help' ] || [ $1 == '-h' ] || [ $1 == '-?' ]; then
-	echo -e "HELP PAGE TO GO HERE"
-elif [ $1 == '-v' ] || [ $1 == '--version' ]; then
-	echo -e "CDANDLS v$__version__";
-else
-	builtin cd "$1";
-	echo -e "\e[01;37m ";	# Line to make default text white
-	ls -aF --color=always --group-directories-first;
-	# Uncomment line below to add extra line at the bottom
-	# that displays current directory
-	#echo -e "\n\e[01;05;33m[ 📁: `dirs` ]\e[m\n";
-	echo -e "\e[m ";	# Line to reset default text color
-fi
+	builtin cd $*
+	\ls -Fa -C -w $COLUMNS --color=always --group-directories-first
 }
 
-# CHANGES
-#
-# version 1.1
-#	- added options: --help, -v
-#
-# version 1.0
-#	- inital function
-
-# TO DO
-# 	- add install and remove option
